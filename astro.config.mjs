fe871eb61278b1150cd96d5282d75a766750611c
IseLein <iseoluwagbotemidaniel@gmail.com>
2025-07-14 07:17:36 -0400

    allowed hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ncmisr.com',
 	integrations: [mdx(), sitemap()],
+	server: {
+		allowedHosts: ['ncmisr.com', 'localhost'],
+	}
 });
